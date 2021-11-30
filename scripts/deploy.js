f653bbf28630c420c9671945e6a2d0f39ab6f43d
Fabian Piper <fabianpiper@web.de>
2021-11-30 11:29:55 +0100

    Add fa2 example

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,50 @@
-const { TezosToolkit } = require("@taquito/taquito");
-const { importKey, InMemorySigner } = require("@taquito/signer");
+const { TezosToolkit, MichelsonMap } = require("@taquito/taquito");
+const { importKey } = require("@taquito/signer");
 const fs = require("fs");
+require("dotenv").config();
+
+const SANDBOX_URL = "http://127.0.0.1:20000";
+const TESTNET_URL = "https://granadanet.api.tez.ie";
+const FAUCET = JSON.parse(process.env.FAUCET);
+const ALICE_SECRET = process.env.ALICE_SECRET;
 
 const deploy = async () => {
   const mode = process.argv[2] || "sandbox";
   console.log(">>> Deploy with mode:", mode);
 
   try {
-    const config = JSON.parse(
-      fs.readFileSync(`./config/${mode}.config.json`).toString()
+    const Tezos = new TezosToolkit(
+      mode === "sandbox" ? SANDBOX_URL : TESTNET_URL
     );
 
-    const Tezos = new TezosToolkit(config.url);
+    // Signing on testnet mode needs different parameters as on sandbox mode
+    const { email, password, mnemonic, secret } = FAUCET;
+    mode === "sandbox"
+      ? await importKey(Tezos, ALICE_SECRET)
+      : await importKey(Tezos, email, password, mnemonic.join(" "), secret);
 
-    if (mode === "testnet") {
-      const { email, password, mnemonic, secret } = config.faucet;
-      await importKey(Tezos, email, password, mnemonic.join(" "), secret);
-    } else {
-      Tezos.setProvider({
-        signer: await InMemorySigner.fromSecretKey(config.alice.secret),
-      });
-    }
+    const code = fs.readFileSync("./contracts/out/fa2_nft_asset.tz").toString();
+    console.log("Originate...");
+    const storage = {
+      admin: {
+        admin: await Tezos.signer.publicKeyHash(), // contract admin set as originating account
+        paused: false,
+      },
+      assets: {
+        ledger: new MichelsonMap(),
+        operators: new MichelsonMap(),
+        metadata: {
+          token_defs: [],
+          next_token_id: 1,
+          metadata: new MichelsonMap(),
+        },
+      },
+      metadata: new MichelsonMap(),
+    };
 
-    const code = fs.readFileSync("./build/counter.tz").toString();
-    console.log("Originate");
     const op = await Tezos.contract.originate({
       code,
-      init: `0`,
+      storage,
     });
 
     console.log("Awaiting confirmation...");
